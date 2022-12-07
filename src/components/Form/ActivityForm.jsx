351d2da78993b41d341847b36f6449528b13d9a5
Padarat Madlee <77786747+bbellducky@users.noreply.github.com>
2022-12-07 16:42:35 +0700

    Update ActivityForm.jsx

--- src/components/Form/ActivityForm.jsx
+++ src/components/Form/ActivityForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-const ActivityForm = ({onAdd}) => {
+const ActivityForm = ({ onAdd }) => {
     const [activityName, setActivityName] = useState('')
     const [activityType, setActivityType] = useState([])
     const [duration, setDuration] = useState([])
@@ -9,14 +9,14 @@ const ActivityForm = ({onAdd}) => {
     const onSubmit = (e) => {
         e.preventDefault()
         console.log(activityName, activityType, description)
-        
+
 
         if (!activityName) {
             alert('Please add a task')
-            return 
+            return
         }
 
-        onAdd({activityName, activityType, duration, description})
+        onAdd({ activityName, activityType, duration, description })
         setActivityName('')
         setActivityType('')
         setDuration('')
@@ -24,25 +24,44 @@ const ActivityForm = ({onAdd}) => {
     }
 
     return (
-        <div className="lg flex justify-center">
+        <div className="lg flex justify-center px-4 py-4">
             <form onSubmit={onSubmit}>
-                <input type="text" name="activityName" value={activityName} placeholder="Activity Name" onChange={(e) => setActivityName(e.target.value)}  className ="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+                Add Activity Form
+                <div className="px-4 py-4">
+                    <label>Activity Name</label>
+                    <br />
+                    <input type="text" name="activityName" value={activityName} placeholder="Activity Name" onChange={(e) => setActivityName(e.target.value)} className="input input-bordered w-full max-w-xs" />
+                </div>
 
-                <select name="activityType" value={activityType} onChange={(e) => setActivityType(e.target.value)} className="select select-bordered w-full max-w-xs" >
-                    <option disabled selected>Please select activity type</option>
-                    <option>Run</option>
-                    <option>Ride</option>
-                    <option>Walk</option>
-                    <option>Hike</option>
-                </select>
+                <div className="px-4 py-4">
+                    <label>Type</label><br />
+                    <select name="activityType" value={activityType} onChange={(e) => setActivityType(e.target.value)} className="select select-bordered w-full max-w-xs" >
+                        <option disabled selected>Please select activity type</option>
+                        <option>Run</option>
+                        <option>Ride</option>
+                        <option>Walk</option>
+                        <option>Hike</option>
+                    </select>
+                </div>
 
-                <input type="text" value={duration} placeholder="Duration" onChange={(e) => setDuration(e.target.value)} className ="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+                <div className="px-4 py-4">
+                    <label>Duration</label><br />
+                    <input type="text" value={duration} placeholder="Duration" onChange={(e) => setDuration(e.target.value)} className="input input-bordered w-full max-w-xs" />
+                </div>
 
-                <input type="text" name="description" value={description} placeholder="description" onChange={(e) => setDescription(e.target.value)} className ="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
-                <button>Add</button>
+                <div className="px-4 py-4">
+                    <label>Description</label><br/>
+                    <input type="text" name="description" value={description} placeholder="description" onChange={(e) => setDescription(e.target.value)} className="input input-bordered w-full max-w-xs" />    
+                </div>
+                
+                <div className="px-4 py-4">
+                <button className="btn btn-wide">Add</button>
+                </div>
+                
             </form>
         </div>
     )
 }
 
 export default ActivityForm
+
