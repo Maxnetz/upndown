c8749cbae842fcdfbf339603e1fa8a3413f1397f
Maxnetz <tanapongt9496@gmail.com>
2022-12-08 00:06:39 +0700

    Chore: Dashboard update

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,12 @@
-import logo from '../../pictures/logo.png';
+import logo from './pictures/logo.png';
 
 const NavBar = () => {
     return (
-    <div className=" h-24 flex justify-between border-b-2 shadow-sm">
+    <div className="bg-white fixed z-10 w-screen h-24 flex justify-between border-b-2 shadow-sm">
         <div className="h-full ml-6 flex items-center">
             <a href="/#"><img src={logo} alt="logo" className="h-16" /></a>
         </div>
-        <div className="h-full mr-6 flex justify-around items-center gap-6 text-purple-500 font-bold text-lg ">
+        <div className="h-full mr-6 flex justify-around items-center gap-6 font-bold text-lg ">
             <a href="/#">Dashboard</a>
             <a href="/#">Create Activity</a>
             <a href="/#">Profile</a>
