cc2811657a471283d1e0fed10d60fdbd2d839bd6
Tanapong <102405834+Maxnetz@users.noreply.github.com>
2022-12-08 00:12:20 +0700

    Revert "001 activity card"

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import NavBar from "./components/NavBar/NavBar";
-import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      
-        {/* <NavBar /> */}
-        <Dashboard />
-        
+    <div className="">
+      <NavBar />
     </div>
-   
   );
 }
 
