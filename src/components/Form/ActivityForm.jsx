3baee272e24b3afe8318134f334dd9b7984a96f9
Padarat Madlee <padarat.madlee@gmail.com>
2022-12-13 10:44:43 +0700

    Change file

--- src/components/Form/ActivityForm.jsx
+++ src/components/Form/ActivityForm.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react"
-import Modal from 'react-modal';
 import logo from './pictures/logo.png'
 const ActivityForm = ({ onAdd }) => {
     const [activityName, setActivityName] = useState('')
