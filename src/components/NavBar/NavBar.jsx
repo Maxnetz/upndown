b7eb0be570903c3fd37e0115c43809a95755b689
Maxnetz <tanapongt9496@gmail.com>
2022-12-15 23:45:21 +0700

    Fix Navbar Register Login State

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
                 <div className="h-full ml-6 flex items-center">
                     <a href="/"><img src={logo} alt="logo" className="h-16" /></a>
                 </div>
-                <div className="h-full mr-6 flex justify-around items-center gap-6 text-purple-500 font-bold text-lg ">
+                <div className="h-full mr-6 flex justify-around items-center gap-8 text-purple-500 font-bold text-lg ">
                     <nav>
                         <Link to="/" className='uppercase  mr-2'>Home</Link>
                         <Link to="/dashboard" className='uppercase  mr-2'>Dashboard</Link>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,93 +1,212 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const Register = () => {
-  return (
-    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-      <div className='bg-white py-8 px-6 shadow rounded-lg sm:px-10'>
-        <form className="mb-0 space-y-6">
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [rePassword, setRePassword] = useState("");
+    const [dateOfBirth, setDateOfBirth] = useState("");
+    const [gender, setGender] = useState("");
+    const [error, setError] = useState(null);
 
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Register</h2>
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    };
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
 
-          <div>
-            <label className="block text-sm font-medium text-gray-700 text-start py-4">Name</label>
-            <div>
-              <input type="text" placeholder="Name" className="w-full border bg-white border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500" />
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+    const handleConfirmPassword = (e) => {
+        setRePassword(e.target.value);
+    };
+    const handleDateOfBirthChange = (e) => {
+        setDateOfBirth(e.target.value);
+    };
+
+    const handleGenderChange = (e) => {
+        setGender(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Validate the form values
+        if (
+            !name ||
+            !email ||
+            !password ||
+            !username ||
+            !dateOfBirth ||
+            !gender
+        ) {
+            setError("All fields are required");
+            return;
+        }
+
+        const emailRegex =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!emailRegex.test(email)) {
+            setError("Please enter a valid email address");
+            return;
+        }
+
+        if (password.length < 8) {
+            setError("Password must be at least 8 characters long");
+            return;
+        }
+
+        // reset state
+        setName("");
+        setEmail("");
+        setUsername("");
+        setPassword("");
+        setRePassword("");
+        setDateOfBirth("");
+        setGender("");
+        setError(null);
+    };
+
+    return (
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div className="bg-white py-8 px-6 shadow rounded-lg sm:px-10">
+                <form className="mb-0 space-y-6" onSubmit={handleSubmit}>
+                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                        Register
+                    </h2>
+
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700 text-start py-4">
+                            Name
+                        </label>
+                        <div>
+                            <input
+                                type="text"
+                                name="Name"
+                                placeholder="Name"
+                                value={name}
+                                onChange={handleNameChange}
+                                className="w-full border bg-white border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700 text-start py-4">
+                            Email
+                        </label>
+                        <div>
+                            <input
+                                type="text"
+                                placeholder="Email"
+                                value={email}
+                                onChange={handleEmailChange}
+                                className="w-full border bg-white border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700 text-start py-4">
+                            Username
+                        </label>
+                        <div>
+                            <input
+                                type="text"
+                                placeholder="Username"
+                                value={username}
+                                onChange={handleUsernameChange}
+                                className="w-full border bg-white border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700 text-start py-4">
+                            Password
+                        </label>
+                        <div>
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                value={password}
+                                onChange={handlePasswordChange}
+                                className="w-full border bg-white border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700 text-start py-4">
+                            Confirm Password
+                        </label>
+                        <div>
+                            <input
+                                type="password"
+                                name="repassword"
+                                id="repassword"
+                                value={rePassword}
+                                onChange={handleConfirmPassword}
+                                className="w-full border bg-white border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700 text-start py-4">
+                            Date of Birth
+                        </label>
+                        <input
+                            type="date"
+                            placeholder="Select date"
+                            value={dateOfBirth}
+                            onChange={handleDateOfBirthChange}
+                            className="w-full border bg-white border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500"
+                        />
+                    </div>
+
+                    <div>
+                        <label
+                            htmlFor="gender"
+                            className="block text-sm font-medium text-gray-700 text-start"
+                        >
+                            Gender:
+                        </label>
+
+                        <div className="mt-1">
+                            <select
+                                id="gender"
+                                name="gender"
+                                value={gender}
+                                onChange={handleGenderChange}
+                                className="select select-bordered w-full border bg-white border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500"
+                            >
+                                <option value="">Select</option>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                                <option value="other">Other</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    {error && <p className="py-4 text-red-600 ">{error}</p>}
+
+                    <button className="focus:outline-none text-white bg-purple-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-800 dark:focus:ring-indigo-400">
+                        Register
+                    </button>
+                </form>
             </div>
-          </div>
+        </div>
+    );
+};
 
-          <div>
-            <label className="block text-sm font-medium text-gray-700 text-start py-4">Email</label>
-            <div>
-              <input type="text" placeholder="Email" className="w-full border bg-white border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500" />
-            </div>
-          </div>
-
-          <div>
-            <label className="block text-sm font-medium text-gray-700 text-start py-4">Username</label>
-            <div>
-              <input type="text" placeholder="Username" className="w-full border bg-white border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500" />
-            </div>
-          </div>
-
-          <div>
-            <label className="block text-sm font-medium text-gray-700 text-start py-4">Password</label>
-            <div>
-              <input type="password" name="password" id="password" className="w-full border bg-white border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500" />
-            </div>
-          </div>
-
-          <div>
-            <label className="block text-sm font-medium text-gray-700 text-start py-4">Confirm Password</label>
-            <div>
-              <input type="cfpassword" name="cfpassword" id="cfpassword" className="w-full border bg-white border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500" />
-            </div>
-          </div>
-
-
-          <div>
-            <label className="block text-sm font-medium text-gray-700 text-start py-4">Date of Birth</label>
-            <input type="date" placeholder="Select date" className="w-full border bg-white border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500" />
-          </div>
-
-
-
-
-          <div>
-            <label className="block text-sm font-medium text-gray-700 text-start py-4">Gender</label>
-            <div className='flex'>
-              <div className='form-check'>
-                <input type="radio" className='form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer' />
-                <label className="form-check-label inline-block text-gray-800 px-2">
-                  Male
-                </label>
-              </div>
-
-              <div className='form-check'>
-                <input type="radio" className='form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer' />
-                <label className="form-check-label inline-block text-gray-800 px-2">
-                  Female
-                </label>
-              </div>
-
-              <div className='form-check'>
-                <input type="radio" className='form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer' />
-                <label className="form-check-label inline-block text-gray-800 px-2">
-                  Others
-                </label>
-              </div>
-            </div>
-          </div>
-
-          {/*ion-datetime*/}
-
-
-
-          <button className="focus:outline-none text-white bg-purple-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-800 dark:focus:ring-indigo-400">Register</button>
-
-        </form>
-      </div>
-    </div>
-  )
-}
-
-export default Register
+export default Register;
